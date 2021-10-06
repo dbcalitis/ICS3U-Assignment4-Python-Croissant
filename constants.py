51d067db225ffb18453f52453bceccbdf896dfca
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-10-06 04:16:12 +0000

    added croissant.py and constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created by: Daria Bernice Calitis
+# Created on: Oct 2021
+# This program is the constants of croissant.py
+
+CROISSANT_PRICE = 2.55
+HST = 1.13
---
+++ croissant.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Created by: Daria Bernice Calitis
+# Created on: Oct 2021
+# This program is the croissant store
+
+import constants
+
+
+def main():
+    # This program calculates the total cost of the croissants
+    total = 0
+    print("Croissants are ${0} each.".format(constants.CROISSANT_PRICE))
+    number_of_croissants = input("How many croissants would you like?: ")
+
+    try:
+        number_of_croissants = int(number_of_croissants)
+        # if the user buys 6 or more croissants then they will get the tax off
+        if number_of_croissants >= 6:
+            total = number_of_croissants * constants.CROISSANT_PRICE
+            print("The total cost will be ${:,.2f} (no HST)".format(total))
+        else:
+            total = (number_of_croissants * constants.CROISSANT_PRICE) * constants.HST
+            print("The total cost will be ${:,.2f} (with HST)".format(total))
+    except (Exception):
+        print("Invalid Input.")
+
+
+if __name__ == "__main__":
+    main()
